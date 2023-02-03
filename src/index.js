72201ed582afaffa6a5a99d5043cd1ae5485757a
Theerapon.r <threerapon.r@thairathonline.co.th>
2023-02-03 14:30:35 +0700

    remove unused

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Material Dashboard React - v1.10.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
@@ -24,6 +7,7 @@ import Admin from "layouts/Admin.js";
 import Market from "layouts/Market.js";
 
 import "assets/css/material-dashboard-react.css?v=1.10.0";
+
 
 ReactDOM.render(
   <BrowserRouter>
